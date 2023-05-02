c6dd2f4a168240732bb3d32de55042949916f012
jmmuozc <josemiguelmunoz013@gmail.com>
2023-05-02 23:42:46 +0200

    changed name of file

--- MVC/BaradosController.js
+++ MVC/BaradosController.js
@@ -4,7 +4,8 @@ import { createClient } from '@supabase/supabase-js'
 const supabase = createClient('https://wcqazpjgyqxtpiytqezb.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndjcWF6cGpneXF4dHBpeXRxZXpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODIyNTI1NDcsImV4cCI6MTk5NzgyODU0N30.4G_KX9VntK8GeNGZpAdM7CcxqN264hySeaHw3pe5fHg');
 
 class BaradosController{
-
+    #baradosModel;
+    #baradosView;
     constructor(baradosModel, baradosView) {
         this.#baradosModel = baradosModel;
         this.#baradosView = baradosView;
