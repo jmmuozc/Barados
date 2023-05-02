606c2ca4e0fd8b47446d4d7de12ca82558c67c2c
jmmuozc <josemiguelmunoz013@gmail.com>
2023-05-02 23:50:45 +0200

    changed bad import

--- MVC/BaradosController.js
+++ MVC/BaradosController.js
@@ -1,5 +1,5 @@
 "use strict"
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 // Crear un cliente para interactuar con la bbdd de supabase
 const supabase = createClient('https://wcqazpjgyqxtpiytqezb.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndjcWF6cGpneXF4dHBpeXRxZXpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODIyNTI1NDcsImV4cCI6MTk5NzgyODU0N30.4G_KX9VntK8GeNGZpAdM7CcxqN264hySeaHw3pe5fHg');
 
