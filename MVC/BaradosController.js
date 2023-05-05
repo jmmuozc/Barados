c13ecaf4300d334c928e80daa1a4839620aba708
jmmuozc <josemiguelmunoz013@gmail.com>
2023-05-05 12:39:38 +0200

    Connection test

--- MVC/BaradosController.js
+++ MVC/BaradosController.js
@@ -1,11 +1,11 @@
 "use strict"
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 // Crear un cliente para interactuar con la bbdd de supabase
-const supabase = createClient('https://wcqazpjgyqxtpiytqezb.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndjcWF6cGpneXF4dHBpeXRxZXpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODIyNTI1NDcsImV4cCI6MTk5NzgyODU0N30.4G_KX9VntK8GeNGZpAdM7CcxqN264hySeaHw3pe5fHg');
 
-class BaradosController{
+class BaradosController {
     #baradosModel;
     #baradosView;
+    #supabaseConection = createClient('https://wcqazpjgyqxtpiytqezb.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndjcWF6cGpneXF4dHBpeXRxZXpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODIyNTI1NDcsImV4cCI6MTk5NzgyODU0N30.4G_KX9VntK8GeNGZpAdM7CcxqN264hySeaHw3pe5fHg');
     constructor(baradosModel, baradosView) {
         this.#baradosModel = baradosModel;
         this.#baradosView = baradosView;
@@ -14,9 +14,17 @@ class BaradosController{
         this.onLoad();
 
         // this.onInit();
-
+        console.log(this.#supabaseConection);
         // Enlazamos handlers con la vista
         this.#baradosView.bindInit(this.handleInit);
+    }
+
+    onLoad = () => {
+
+        // const { data, error } = await this.#supabaseConection
+        //     .from('Business')
+        //     .select()
+
     }
 }
 
