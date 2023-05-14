dfec9dd79e5bd07d91987bbd3bf1aefb20aeeb88
jmmuozc <josemiguelmunoz013@gmail.com>
2023-05-14 18:24:59 +0200

    added event to login form

---
+++ MVC/validation.js
@@ -0,0 +1,44 @@
+"use strict";
+
+function showFeedBack(input, valid, message) {
+    let validClass = (valid) ? 'is-valid' : 'is-invalid';
+    let div = (valid) ? input.nextAll("div.valid-feedback") : input.nextAll("div.invalid-feedback");
+    input.nextAll('div').removeClass('d-block');
+    div.removeClass('d-none').addClass('d-block');
+    input.removeClass('is-valid is-invalid').addClass(validClass);
+    if (message) {
+        div.empty();
+        div.append(message);
+    }
+}
+
+function logInValidation(handler) {
+    let form = document.forms.logIn;
+    $(form).attr('novalidate', true);
+    $(form).submit(function (event) {
+        let isValid = true;
+        let firstInvalidElement = null;
+        this.email.value = this.email.value.trim();
+        if (this.email.value == "") {
+            showFeedBack($(this.email), false);
+        } else {
+            showFeedBack($(this.email), true);
+        }
+
+        if (!this.passwd.checkValidity()) {
+            isValid = false;
+            showFeedBack($(this.passwd), false);
+            firstInvalidElement = this.passwd;
+        } else {
+            showFeedBack($(this.passwd), true);
+        }
+
+        if (!isValid) {
+            firstInvalidElement.focus();
+        } else {
+            handler(this.email.value, this.passwd.value);
+        }
+        event.preventDefault();
+        event.stopPropagation();
+    });
+}
