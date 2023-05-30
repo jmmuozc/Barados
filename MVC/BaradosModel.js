ff44f54ba6b15863aa30faf365cf5b52de6dd15a
jmmuozc <josemiguelmunoz013@gmail.com>
2023-05-31 01:34:23 +0200

    adding features

--- MVC/BaradosModel.js
+++ MVC/BaradosModel.js
@@ -1,7 +1,11 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+import Owner from '../Js/Owner.js';
+import Customer from "../Js/Customer.js";
+import Business from "../Js/Business.js";
 class Barados {
 
   #supabaseConnection = createClient('https://wcqazpjgyqxtpiytqezb.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndjcWF6cGpneXF4dHBpeXRxZXpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODIyNTI1NDcsImV4cCI6MTk5NzgyODU0N30.4G_KX9VntK8GeNGZpAdM7CcxqN264hySeaHw3pe5fHg');
+  #currentUser;
 
   fetchData = async (table) => {
 
@@ -21,12 +25,12 @@ class Barados {
     // return JSON.parse(JSON.stringify(data));
   }
 
-  fetchDataWhere = async (table,condition) => {
+  fetchDataWhere = async (table, condition) => {
 
-    console.log(condition);
+    // console.log(condition);
     const { data, error } = await this.#supabaseConnection.from(table).select().match(condition);
 
-      //  console.log(fetched.data);
+    //  console.log(fetched.data);
     // for (let data of fetched.data) {
     //     data=JSON.stringify(data);
     // }
@@ -36,28 +40,33 @@ class Barados {
 
     return JSON.parse(JSON.stringify(data));
   }
-  
-  insertInto = async (table, data) => {
 
-    const { error } = await this.#supabaseConnection
+  insertInto = async (table, newData) => {
+
+    const {data, error } = await this.#supabaseConnection
       .from(table)
-      .insert(data)
+      .insert(newData)
+      .select(Id);
 
     if (error) {
       return error;
+    }
+
+    if (data) {
+      return JSON.parse(JSON.stringify(data));
     }
   }
 
   logIn = async (email, passwd) => {
 
     const { data, error } = await this.#supabaseConnection.auth.signInWithPassword({
-        email: email,
-        password: passwd,
-      });
+      email: email,
+      password: passwd,
+    });
 
 
 
-    
+
     if (error == null) {
       return data.user["email"];
       // return JSON.stringify(data.user["email"]);
@@ -80,11 +89,12 @@ class Barados {
 
     const { data, error } = await this.#supabaseConnection.auth.getSession();
 
-    if (data.session==null) {
+    if (data.session == null) {
       return false;
     }
 
     if (data) {
+      // return data.session["user"].email;
       return data.session["user"].email;
     }
 
@@ -106,14 +116,47 @@ class Barados {
     }
   }
 
-  getBase64FomFile(img, callback){
+  getBase64FromFile(img, callback) {
     let fileReader = new FileReader();
-    fileReader.addEventListener('load' ,function(evt){
+    fileReader.addEventListener('load', function (evt) {
       callback(fileReader.result);
     });
     fileReader.readAsDataURL(img);
+    return fileReader.result;
+  }
+  getBase64(file) {
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function () {
+      // console.log(reader.result);
+      return reader.result;
+    };
+    reader.onerror = function (error) {
+      console.log('Error: ', error);
+    };
+
   }
 
+  toBase64(file) {
+    const reader = new FileReader();
+    let reject;
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = reject;
+    console.log(reader.result);
+  }
+
+
+  OwnerFactory(id,name){
+    this.#currentUser=new Owner(id,name);
+  }
+
+  CustomerFactory(id,name){
+    this.#currentUser=new Customer(id,name);  
+  }
+  BusinessFactory(id,name){
+    this.#currentUser=new Business(id,name);
+  }
 }
 
 export default Barados;
